5e9eef59e72e02936fffbf3772203e9ce88cbc01
leumij <cjimuel74@gmail.com>
2024-01-26 13:45:38 +0800

    for even index

--- charactersfromstring.py
+++ charactersfromstring.py
@@ -1,7 +1,12 @@
 # Write a program to accept a string from the user and display characters that are present at an even index number.
 
-# pseudcode
-# Ask user for Word
-# Use for range
-# Then print for range
+# pseudocode
 
+# Ask user for Word
+ask = input("Enter your Word: ")
+
+# Use for range
+for i in ask[1::2]:
+
+# Then print for range
+    print(i)
